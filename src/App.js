9a616304c0c96772bd3bfe3cefbe167aae84d684
Nika <nikaonime@gmail.com>
2023-05-29 10:35:14 +0300

    form create post

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import { PostList } from "./Components/PostList";
 
 function App() {
 
-  const [posts1, setPosts1] = useState([
+  const [posts, setPosts] = useState([
     { id: 1, title: 'JS', body: 'JS - it is programming language' },
     { id: 2, title: 'JS', body: 'JS - it is programming language' },
     { id: 3, title: 'JS', body: 'JS - it is programming language' }
   ])
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'python', body: 'python - it is programming language' },
-    { id: 2, title: 'python', body: 'python - it is programming language' },
-    { id: 3, title: 'python', body: 'python - it is programming language' }
-  ])
-
   return (
     <div className="App">
-      <PostList posts={posts1} title='post list js' />
-      <PostList posts={posts2} title='post list python' />
+      <form>
+        <input type='text' placeholder="desc post title" />
+        <input type='text' placeholder="desc post" />
+        <button>create post</button>
+      </form>
+      <PostList posts={posts} title='post list'/>
     </div>
   );
 }
