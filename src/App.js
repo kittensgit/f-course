b848e0ed3c430a16fd8bc0ee0dbd43e32870d8df
Nika <nikaonime@gmail.com>
2023-05-29 09:28:54 +0300

    onChange, input, value

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function App() {
 
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('text')
 
   const increment = () => {
     setLikes((actual) => actual + 1)
@@ -12,10 +13,16 @@ function App() {
     setLikes((actual) => actual - 1)
   }
 
+  const changeHandler = (e) => {
+    setValue(e.currentTarget.value)
+  }
+
 
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type='text' onChange={changeHandler} value={value} />
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
     </div>
