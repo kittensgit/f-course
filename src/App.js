8387c265b65e2a75bf2a2562ba9784e898930a4d
Nika <nikaonime@gmail.com>
2023-05-29 09:26:34 +0300

    setState, increment, decrement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [likes, setLikes] = useState(0)
+
+  const increment = () => {
+    setLikes((actual) => actual + 1)
+  }
+
+  const decrement = () => {
+    setLikes((actual) => actual - 1)
+  }
+
+
   return (
     <div className="App">
-    
+      <h1>{likes}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
