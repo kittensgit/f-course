dbc5f0558c5d010d7ebe1268ab4225f9aee790d7
Nika <nikaonime@gmail.com>
2023-05-31 10:37:51 +0300

    custom hooks, decomposition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import './styles/App.css';
 import { PostList } from "./Components/PostList";
 import { PostForm } from "./Components/PostForm";
 import { PostFilter } from "./Components/PostFilter";
 import { MyModal } from "./Components/UI/modal/MyModal";
 import { MyButton } from "./Components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
 
@@ -17,16 +18,7 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -39,7 +31,7 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+      <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
         Create post
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
