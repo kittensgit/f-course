cac458a42bc24111a855daa714fb97d06a365500
Nika <nikaonime@gmail.com>
2023-06-01 11:30:41 +0300

    utils/pages.js, pagination, get query params

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { usePosts } from "./hooks/usePosts";
 import PostService from "./Components/API/PostService";
 import { Loader } from "./Components/UI/loader/Loader";
 import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
 
@@ -16,12 +17,19 @@ function App() {
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)  //общее кол-во страниц
+  const [limit, setLimit] = useState(10) // лимит постов
+  const [page, setPage] = useState(1) //номер текущей страницы
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count'] //общее кол-во постов
+    setTotalPages(getPageCount(totalCount, limit))
   })
+
+  let pagesArray = getPagesArray(totalPages) //в массиве лежат числа от 1 до 10
 
   useEffect(() => {
     fetchPosts()
@@ -36,6 +44,11 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts()
+  }
+
   return (
     <div className="App">
       <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
@@ -46,13 +59,24 @@ function App() {
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && 
-      <h1>An error has occurred "{postError}"</h1>
+      {postError &&
+        <h1>An error has occurred "{postError}"</h1>
       }
       {isPostsLoading
         ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='post list' />
       }
+      <div className="page__wrapper">
+        {pagesArray.map(p =>
+          <span
+            onClick={() => changePage(p)}
+            key={p}
+            className={page === p ? 'page page__current' : 'page'}
+          >
+            {p}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
