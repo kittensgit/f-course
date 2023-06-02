329758df79c800db26c0a68ba8fc213465df1c8b
Nika <nikaonime@gmail.com>
2023-06-02 11:18:37 +0300

    fetchComments

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -8,13 +8,20 @@ export const PostIdPage = () => {
 
     const params = useParams()
     const [post, setPost] = useState({})
+    const [comments, setComments] = useState([])
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id) // сервер возвращает нам пост
         setPost(response.data)
     })
 
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentByPostIdPage(id) 
+        setComments(response.data)
+    })
+
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
 
     return (
@@ -23,6 +30,18 @@ export const PostIdPage = () => {
             {isLoading
                 ? <Loader />
                 : <div>{post.id}. {post.title}</div>
+            }
+            <h1>Comments</h1>
+            {isComLoading
+                ? <Loader />
+                : <div>
+                    {comments.map(comm => 
+                        <div style={{ marginTop: '15px' }}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
             }
         </div>
     )
